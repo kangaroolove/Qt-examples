e15b0b5450dab271e01a46065dfc1899a7673a78
PangKuanXin <pang.kuanxin@ultrastmedtech.com>
2024-01-23 17:30:59 +0800

    Submit memoryLeakDetectCRT project

---
+++ MemoryLeakDetectCRT/Widget.cpp
@@ -0,0 +1,12 @@
+#include "Widget.h"
+
+Widget::Widget(QWidget * parent)
+    : QWidget(parent)
+{
+    double *p = new double[10];
+}
+
+Widget::~Widget()
+{
+
+}
---
+++ MemoryLeakDetectCRT/main.cpp
@@ -0,0 +1,27 @@
+#include "Widget.h"
+#include <QApplication>
+
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
+#ifdef _DEBUG
+    #define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+    // Replace _NORMAL_BLOCK with _CLIENT_BLOCK if you want the
+    // allocations to be of _CLIENT_BLOCK type
+#else
+    #define DBG_NEW new
+#endif
+
+int main(int argc, char *argv[])
+{
+    // Multiple exits
+    _CrtSetDbgFlag ( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+
+    QApplication a(argc, argv);
+    Widget widget;
+    widget.show();
+
+    _CrtDumpMemoryLeaks();
+    return a.exec();
+}
