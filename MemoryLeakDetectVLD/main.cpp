1336e1ab321db089395b39c716a303c4211d22f3
PangKuanXin <pang.kuanxin@ultrastmedtech.com>
2024-01-24 10:57:27 +0800

    Sort the code

--- MemoryLeakDetectVLD/main.cpp
+++ MemoryLeakDetectVLD/main.cpp
@@ -2,6 +2,9 @@
 #include "Widget.h"
 #include <QApplication>
 
+// Before you run this application, make sure you have installed VLD.
+// Because it will read the vld.ini file
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
