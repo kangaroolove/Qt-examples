402f5bb498128ac9c89c6e21626d3e28ebf61737
PangKuanXin <pang.kuanxin@ultrastmedtech.com>
2024-03-03 10:07:42 +0800

    Submit Daqc server class

---
+++ LocalIPCAdvanced/server/DaqcServer.cpp
@@ -0,0 +1,11 @@
+#include "DaqcServer.h"
+
+DaqcServer::DaqcServer()
+{
+
+}
+
+DaqcServer::~DaqcServer()
+{
+
+}
---
+++ LocalIPCAdvanced/server/DaqcServer.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Server.h"
+
+class DaqcServer : public Server
+{
+public:
+    DaqcServer();
+    ~DaqcServer();
+
+private:
+
+};
