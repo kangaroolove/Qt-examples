6882d415679fbfa7ce81b0904b091e061b915951
PangKuanXin <pang.kuanxin@ultrastmedtech.com>
2024-03-09 19:37:04 +0800

    Add ParameterDef.h

---
+++ LocalIPCAdvanced/common/ParameterDef.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <QString>
+
+const QString REGION_PHYSICAL_DELTA_X = "RegionPhysicalDeltaX";
+const QString REGION_PHYSICAL_DELTA_Y = "RegionPhysicalDeltaY";
+const QString B_GAIN = "BGain";
+const QString C_GAIN = "CGain";
+const QString M_GAIN = "MGain";
+const QString XML_DEPTH = "XmlDepth";
+const QString DEPTH_MM = "DepthMm";
+const QString DEPTH_CM = "DepthCm";
+const QString B_FREQUENCY = "BFrequency";
+const QString B_FREQUENCY_2 = "BFrequency2";
+const QString M_FREQUENCY = "MFrequency";
+const QString CHROMA = "Chroma";
+const QString IMG_PROC = "ImgProc";
+const QString FAVG = "Favg";
+const QString LAVG = "Lavg";
+const QString CONTRAST = "Contrast";
+const QString C_PRF = "CPrf";
+const QString D_PRF = "DPrf";
+const QString C_WF = "CWF";
+const QString D_WF = "DWF";
+const QString SENSITIVITY = "Sensitivity";
+const QString SWING_ANGLE = "SwingAngle";
+const QString SV = "Sv";
+const QString CA = "Ca";
+const QString D_SPEED = "DSpeed";
+const QString M_SPEED = "MSpeed";
+const QString NOISE_REJECT = "NoiseReject";
+const QString AUDIO = "Audio";
+const QString BASELINE = "BaseLine";
+const QString ROTATION = "Rotation";
+const QString SPACING_X = "SpacingX";
+const QString SPACING_Y = "SpacingY";
