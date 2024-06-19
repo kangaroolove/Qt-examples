a4c82b258f9162965fa74d581b4aa77cbd81258b
PangKuanXin <pang.kuanxin@ultrastmedtech.com>
2024-06-19 16:19:37 +0800

    Add auto send logic

--- LocalIPC/server/ServerWidget.cpp
+++ LocalIPC/server/ServerWidget.cpp
@@ -5,14 +5,20 @@
 #include <QLabel>
 #include <QPushButton>
 #include <QDebug>
+#include <QLineEdit>
+#include <QHBoxLayout>
+#include <QTimer>
+#include <QMessageBox>
 
-ServerWidget::ServerWidget(QWidget* parent)
-    : QWidget(parent)
+ServerWidget::ServerWidget(QWidget* parent) : 
+QWidget(parent)
     , m_receiveTextEdit(new QTextEdit(this))
     , m_sendTextEdit(new QTextEdit(this))
     , m_sendButton(new QPushButton("Send", this))
     , m_statusLabel(new QLabel(this))
     , m_server(new Server(this))
+    , m_timerIntervalInput(new QLineEdit(this))
+    , m_autoSendButton(new QPushButton("Auto send"))
 {
     initGui();
     bindConnections();
@@ -24,11 +30,47 @@ ServerWidget::~ServerWidget()
 
 }
 
+void ServerWidget::onAutoSendButtonClicked()
+{
+    auto text = m_sendTextEdit->toPlainText();
+    if (text.isEmpty())
+    {
+        QMessageBox::critical(this, "Critical", "You didn't type anything!");
+        return;
+    }
+
+    auto interval = m_timerIntervalInput->text();
+    if (interval.isEmpty())
+    {
+        QMessageBox::critical(this, "Critical", "You didn't enter the timer interval!");
+        return;
+    }
+
+    m_timer = std::make_unique<QTimer>();
+    m_timer->setInterval(interval.toInt());
+    connect(m_timer.get(), &QTimer::timeout, this, &ServerWidget::onSendButtonClicked);
+    m_timer->start();
+}
+
+void ServerWidget::onSendButtonClicked()
+{
+    auto text = m_sendTextEdit->toPlainText();
+    if (text.isEmpty())
+    {
+        QMessageBox::critical(this, "Critical", "You didn't type anything!");
+        return;
+    }
+    
+    m_server->sendMessage(text);
+}
+
 void ServerWidget::initGui()
 {
     QVBoxLayout *layout = new QVBoxLayout(this);
     QLabel* receiveLabel = new QLabel("Receive:", this);
     QLabel* sendLabel = new QLabel("Send:", this);
+
+    m_timerIntervalInput->setPlaceholderText("ms");
 
     layout->addWidget(m_statusLabel);
     layout->addWidget(receiveLabel);
@@ -36,6 +78,7 @@ void ServerWidget::initGui()
     layout->addWidget(sendLabel);
     layout->addWidget(m_sendTextEdit);
     layout->addWidget(m_sendButton);
+    layout->addLayout(getTimerLayout());
 }
 
 void ServerWidget::startServer()
@@ -48,11 +91,19 @@ void ServerWidget::startServer()
 
 void ServerWidget::bindConnections()
 {
-    connect(m_sendButton, &QPushButton::clicked, this, [this]{
-        m_server->sendMessage(m_sendTextEdit->toPlainText());
-        m_sendTextEdit->clear();
-    });
+    connect(m_sendButton, &QPushButton::clicked, this, &ServerWidget::onSendButtonClicked);
     connect(m_server, &Server::receiveMessage, this, [this](const QString& msg){
         m_receiveTextEdit->append(msg);
     });
+    connect(m_autoSendButton, &QPushButton::clicked, this, &ServerWidget::onAutoSendButtonClicked);
+}
+
+QHBoxLayout *ServerWidget::getTimerLayout()
+{
+    auto layout = new QHBoxLayout();
+    auto timerLabel = new QLabel("Sending gap:");
+    layout->addWidget(timerLabel);
+    layout->addWidget(m_timerIntervalInput);
+    layout->addWidget(m_autoSendButton);
+    return layout;
 }
--- LocalIPC/server/ServerWidget.h
+++ LocalIPC/server/ServerWidget.h
@@ -1,26 +1,35 @@
 #pragma once
 
 #include <QWidget>
+#include <memory>
 
 class QTextEdit;
 class QPushButton;
 class QLabel;
 class Server;
+class QLineEdit;
+class QHBoxLayout;
 
 class ServerWidget : public QWidget
 {
 public:
     ServerWidget(QWidget* parent = nullptr);
     ~ServerWidget();
-
+private slots:
+    void onAutoSendButtonClicked();
+    void onSendButtonClicked();
 private:
     void initGui();
     void startServer();
     void bindConnections();
+    QHBoxLayout* getTimerLayout();
+    std::unique_ptr<QTimer> m_timer;
 
     QTextEdit* m_receiveTextEdit;
     QTextEdit* m_sendTextEdit;
     QPushButton* m_sendButton;
+    QPushButton* m_autoSendButton;
     QLabel* m_statusLabel;
     Server* m_server;
+    QLineEdit* m_timerIntervalInput;
 };
