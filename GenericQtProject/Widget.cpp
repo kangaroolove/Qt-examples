b73643dc385d532ca0e7d913f025714f37e49eb3
PangKuanXin <pang.kuanxin@ultrastmedtech.com>
2024-11-18 17:01:51 +0800

    Remove useless functions

--- GenericQtProject/Widget.cpp
+++ GenericQtProject/Widget.cpp
@@ -4,25 +4,6 @@
 Widget::Widget(QWidget * parent)
     : QWidget(parent)
 {
-
-    std::vector<int> vector = {
-        1, 2, 3, 4, 5
-    };
-
-    for (auto& item : vector)
-    {
-        qDebug()<<item;
-    }
-
-    qDebug()<<"After";
-    auto it = std::remove(vector.begin(), vector.end(), 3);
-    vector.erase(it);
-    qDebug()<<"distance = "<<std::distance(vector.begin(), it);
-
-    for (auto& item : vector)
-    {
-        qDebug()<<item;
-    }
 }
 
 Widget::~Widget()
