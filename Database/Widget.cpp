697661b6e1a26b0a26d9177212e1de2e04ae0c78
kuanxin <pang.kuanxin@cmsholding.com>
2024-11-23 12:34:00 +0800

    Reformat

--- Database/Widget.cpp
+++ Database/Widget.cpp
@@ -1,67 +1,57 @@
 #include "Widget.h"
 
-#include <QSqlQuery>
-#include <QDebug>
-#include <qsqlerror.h>
 #include <QApplication>
 #include <QDebug>
 #include <QFileInfo>
+#include <QSqlQuery>
+#include <QSqlTableModel>
 #include <QTableView>
 #include <QVBoxLayout>
-#include <QSqlTableModel>
+#include <qsqlerror.h>
 
-Widget::Widget(QWidget * parent)
-    : QWidget(parent)
-    , m_tableView(new QTableView)
-{
-    initGui();
-    auto database = QSqlDatabase::addDatabase("QSQLITE", "my connection");
-    database.setDatabaseName("testDatabase.db");
-    if (!database.open())
-    {
-        qDebug()<<"Cannot open the database";
-        qDebug()<<database.lastError().text();
-        return;
-    }
+Widget::Widget(QWidget *parent) : QWidget(parent), m_tableView(new QTableView) {
+  initGui();
+  auto database = QSqlDatabase::addDatabase("QSQLITE", "my connection");
+  database.setDatabaseName("testDatabase.db");
+  if (!database.open()) {
+    qDebug() << "Cannot open the database";
+    qDebug() << database.lastError().text();
+    return;
+  }
 
-    if (!isDatabaseCreated())
-        createDatabase(database);
+  if (!isDatabaseCreated())
+    createDatabase(database);
 
-    showData(database);
+  showData(database);
 }
 
-Widget::~Widget()
-{
+Widget::~Widget() {}
 
+void Widget::createDatabase(const QSqlDatabase &database) {
+  QStringList cmds;
+  cmds << "CREATE TABLE Users (UserID INTEGER PRIMARY KEY AUTOINCREMENT, "
+          "Username VARCHAR(50) NOT NULL UNIQUE, Email VARCHAR(100) NOT NULL "
+          "UNIQUE);";
+  cmds << ("INSERT INTO Users (Username, Email) VALUES ('a', 'a@gmail.com');");
+  cmds << ("INSERT INTO Users (Username, Email) VALUES ('b', 'b@gmail.com');");
+  QSqlQuery sql(database);
+  for (auto &cmd : cmds)
+    qDebug() << "cmd = " << cmd << ", result = " << sql.exec(cmd);
 }
 
-void Widget::createDatabase(const QSqlDatabase& database)
-{
-    QStringList cmds;
-    cmds<<"CREATE TABLE Users (UserID INTEGER PRIMARY KEY AUTOINCREMENT, Username VARCHAR(50) NOT NULL UNIQUE, Email VARCHAR(100) NOT NULL UNIQUE);";
-    cmds<<("INSERT INTO Users (Username, Email) VALUES ('a', 'a@gmail.com');" );
-    cmds<<("INSERT INTO Users (Username, Email) VALUES ('b', 'b@gmail.com');");
-    QSqlQuery sql(database);  
-    for (auto& cmd : cmds)
-        qDebug()<<"cmd = "<<cmd<<", result = "<<sql.exec(cmd);
+bool Widget::isDatabaseCreated() {
+  QFileInfo fileInfo(QApplication::applicationDirPath() + "/testDatabase.db");
+  return fileInfo.size() > 0 ? true : false;
 }
 
-bool Widget::isDatabaseCreated()
-{
-    QFileInfo fileInfo(QApplication::applicationDirPath() + "/testDatabase.db");
-    return fileInfo.size() > 0 ? true : false;
+void Widget::showData(const QSqlDatabase &database) {
+  auto model = new QSqlTableModel(this, database);
+  model->setTable("Users");
+  model->select();
+  m_tableView->setModel(model);
 }
 
-void Widget::showData(const QSqlDatabase &database)
-{
-    auto model = new QSqlTableModel(this, database);
-    model->setTable("Users");
-    model->select();
-    m_tableView->setModel(model);
-}
-
-void Widget::initGui()
-{
-    auto layout = new QVBoxLayout(this);
-    layout->addWidget(m_tableView);
+void Widget::initGui() {
+  auto layout = new QVBoxLayout(this);
+  layout->addWidget(m_tableView);
 }
--- Database/Widget.h
+++ Database/Widget.h
@@ -1,26 +1,27 @@
 #pragma once
 
-#include <QWidget>
 #include <QSqlDatabase>
+#include <QWidget>
 
 class QTableView;
 
-class Widget : public QWidget
-{
-    Q_OBJECT
+class Widget : public QWidget {
+  Q_OBJECT
 public:
-    Widget(QWidget* parent = nullptr);
-    ~Widget();
-private:
-    void initGui();
-    /**
-     * @brief Create the database and this function should be run when there is no database otherwise QSqlQuery will return false
-     * 
-     * @param database 
-     */
-    void createDatabase(const QSqlDatabase& database);
-    bool isDatabaseCreated();
-    void showData(const QSqlDatabase& database);
+  Widget(QWidget *parent = nullptr);
+  ~Widget();
 
-    QTableView* m_tableView;
+private:
+  void initGui();
+  /**
+   * @brief Create the database and this function should be run when there is no
+   * database otherwise QSqlQuery will return false
+   *
+   * @param database
+   */
+  void createDatabase(const QSqlDatabase &database);
+  bool isDatabaseCreated();
+  void showData(const QSqlDatabase &database);
+
+  QTableView *m_tableView;
 };
