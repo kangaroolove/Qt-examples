bfce7a185f3033e1c80f695cdf5383de8eb84598
kuanxin <pang.kuanxin@cmsholding.com>
2024-11-23 16:46:07 +0800

    Reformat

--- LineChart/mainwindow.cpp
+++ LineChart/mainwindow.cpp
@@ -1,62 +1,53 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
-#include <QLineSeries>
-#include <QDateTime>
-#include <QChartView>
-#include <QDateTimeAxis>
-#include <QValueAxis>
 #include <QChart>
+#include <QChartView>
+#include <QDateTime>
+#include <QDateTimeAxis>
 #include <QDebug>
+#include <QLineSeries>
+#include <QValueAxis>
 
 using namespace QtCharts;
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
+  ui->setupUi(this);
+  QLineSeries *series = new QLineSeries;
+  series->setName("Time");
 
-#if 1
-    QLineSeries *series = new QLineSeries;
-    series->setName("Time");
+  QDateTime xValue;
+  xValue.setDate(QDate(2012, 1, 18));
+  xValue.setTime(QTime(9, 34));
+  qreal yValue = 4;
+  series->append(xValue.toMSecsSinceEpoch(), yValue);
 
-    QDateTime xValue;
-    xValue.setDate(QDate(2012, 1 , 18));
-    xValue.setTime(QTime(9, 34));
-    qreal yValue = 4;
-    series->append(xValue.toMSecsSinceEpoch(), yValue);
+  xValue.setDate(QDate(2013, 5, 11));
+  xValue.setTime(QTime(11, 14));
 
-    xValue.setDate(QDate(2013, 5 , 11));
-    xValue.setTime(QTime(11, 14));
+  qreal yValue2 = 50;
+  series->append(xValue.toMSecsSinceEpoch(), yValue2);
 
-    qreal yValue2 = 50;
-    series->append(xValue.toMSecsSinceEpoch(), yValue2);
+  QChart *chart = new QChart;
+  chart->setAnimationOptions(QChart::SeriesAnimations);
 
-    QChart *chart = new QChart;
-    chart->setAnimationOptions(QChart::SeriesAnimations);
+  QDateTimeAxis *axisX = new QDateTimeAxis;
+  axisX->setFormat("yyyy-MM-dd");
+  axisX->setTickCount(2);
+  axisX->setLabelsAngle(-45);
 
-    QDateTimeAxis *axisX = new QDateTimeAxis;
-    axisX->setFormat("yyyy-MM-dd");
-    axisX->setTickCount(2);
-    axisX->setLabelsAngle(-45);
+  QValueAxis *axisY = new QValueAxis;
+  axisY->setTitleText("fdafdsa");
+  axisY->setRange(0, 100);
 
-    QValueAxis *axisY = new QValueAxis;
-    axisY->setTitleText("fdafdsa");
-    axisY->setRange(0, 100);
+  chart->addSeries(series);
+  chart->setAxisY(axisY, series);
+  chart->setAxisX(axisX, series);
 
-    chart->addSeries(series);
-    chart->setAxisY(axisY, series);
-    chart->setAxisX(axisX, series);
+  QChartView *chartView = new QChartView(chart);
+  chartView->setRenderHints(QPainter::Antialiasing);
 
-    QChartView *chartView = new QChartView(chart);
-    chartView->setRenderHints(QPainter::Antialiasing);
-#endif
-    setCentralWidget(chartView);
-
+  setCentralWidget(chartView);
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
-
+MainWindow::~MainWindow() { delete ui; }
