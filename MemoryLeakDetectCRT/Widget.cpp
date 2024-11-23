460d9d7642267064425ae77936882dfc64604d7e
kuanxin <pang.kuanxin@cmsholding.com>
2024-11-23 16:47:32 +0800

    Reformat

--- MemoryLeakDetectCRT/Widget.cpp
+++ MemoryLeakDetectCRT/Widget.cpp
@@ -1,12 +1,7 @@
 #include "Widget.h"
 
-Widget::Widget(QWidget * parent)
-    : QWidget(parent)
-{
-    double *p = new double[10];
+Widget::Widget(QWidget *parent) : QWidget(parent) {
+  double *p = new double[10];
 }
 
-Widget::~Widget()
-{
-
-}
+Widget::~Widget() {}
