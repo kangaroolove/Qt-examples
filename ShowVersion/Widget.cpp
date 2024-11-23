792859b6a64f253e5e2e21f60d2a73c0949dccdd
kuanxin <pang.kuanxin@cmsholding.com>
2024-11-23 16:56:41 +0800

    Reformat

--- ShowVersion/Widget.cpp
+++ ShowVersion/Widget.cpp
@@ -1,21 +1,19 @@
-#include "VersionConfig.h"
 #include "Widget.h"
+#include "VersionConfig.h"
 #include <QLabel>
-#include <QVBoxLayout>
 #include <QString>
+#include <QVBoxLayout>
 #include <iostream>
 
-Widget::Widget(QWidget * parent)
-    : QWidget(parent)
-{
-    auto layout = new QVBoxLayout(this);
-    auto label = new QLabel;
-    label->setText(QString("Version: %1.%2.%3").arg(VERSION_MAJOR).arg(VERSION_MINOR).arg(VERSION_PATCH));
-    layout->addWidget(label);
-    this->resize(400, 300);
+Widget::Widget(QWidget *parent) : QWidget(parent) {
+  auto layout = new QVBoxLayout(this);
+  auto label = new QLabel;
+  label->setText(QString("Version: %1.%2.%3")
+                     .arg(VERSION_MAJOR)
+                     .arg(VERSION_MINOR)
+                     .arg(VERSION_PATCH));
+  layout->addWidget(label);
+  this->resize(400, 300);
 }
 
-Widget::~Widget()
-{
-
-}
+Widget::~Widget() {}
