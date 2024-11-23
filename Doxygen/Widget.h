9481e1255baa259231478094f4bd4dbfa3882cf3
kuanxin <pang.kuanxin@cmsholding.com>
2024-11-23 16:42:10 +0800

    reformat

--- Doxygen/Widget.h
+++ Doxygen/Widget.h
@@ -1,12 +1,12 @@
 /**
  * @file Widget.h
  * @author your name (you@domain.com)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2024-02-13
- * 
+ *
  * @copyright Copyright (c) 2024
- * 
+ *
  */
 
 #pragma once
@@ -15,32 +15,31 @@
 
 /**
  * @brief This is a basic class
- * 
+ *
  */
 
-class Widget : public QWidget
-{
-    Q_OBJECT
+class Widget : public QWidget {
+  Q_OBJECT
 public:
-    Widget(QWidget* parent = nullptr);
-    ~Widget();
+  Widget(QWidget *parent = nullptr);
+  ~Widget();
 
-    /**
-     * @brief This function is for printing a int
-     * 
-     * @param number 
-     */
-    void printInt(const int& number);
+  /**
+   * @brief This function is for printing a int
+   *
+   * @param number
+   */
+  void printInt(const int &number);
 
-    /**
-     * @brief This function will return a bool
-     * 
-     * @return true If you are right
-     * @return false If you are wrong
-     * @see printInt(const int& number)
-     * @note This is a note
-     * @attention This is an attention
-     * @warning This is a warning
-     */
-    bool isBool();
+  /**
+   * @brief This function will return a bool
+   *
+   * @return true If you are right
+   * @return false If you are wrong
+   * @see printInt(const int& number)
+   * @note This is a note
+   * @attention This is an attention
+   * @warning This is a warning
+   */
+  bool isBool();
 };
