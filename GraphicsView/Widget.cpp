9f3e5add9a45ac1bdff2acb94b86407c7cbdbc67
PangKuanXin <pang.kuanxin@ultrastmedtech.com>
2025-03-07 09:53:32 +0800

    Submit GraphicsView project

---
+++ GraphicsView/Widget.cpp
@@ -0,0 +1,4 @@
+#include "Widget.h"
+#include <QDebug>
+
+Widget::Widget(QWidget *parent) : QWidget(parent) {}
---
+++ GraphicsView/main.cpp
@@ -0,0 +1,24 @@
+#include "Widget.h"
+#include <QApplication>
+#include <QDebug>
+#include <QGraphicsScene>
+#include <QGraphicsTextItem>
+#include <QGraphicsView>
+
+int main(int argc, char *argv[]) {
+  QApplication a(argc, argv);
+
+  QGraphicsScene scene;
+  scene.setSceneRect(0, 0, 200, 200);
+  scene.addText("Hello, world");
+
+  auto text2 = scene.addText("Second Text");
+  text2->setPos(50, 50);
+
+  QGraphicsView view(&scene);
+  view.show();
+
+  qDebug() << scene.sceneRect();
+
+  return a.exec();
+}
