c51b44115f4877868f38fab947edbdb9f6d80b48
Pang kuanxin <pang.kuanxin@cmsholding.com>
2025-05-22 16:07:14 +0800

    Reformat

--- QProperty/MyClass.cpp
+++ QProperty/MyClass.cpp
@@ -5,11 +5,10 @@ MyClass::MyClass(QObject *parent) : QObject(parent), m_visible(false) {}
 MyClass::~MyClass() {}
 
 void MyClass::setVisible(bool visible) {
-  if (visible == m_visible)
-    return;
+    if (visible == m_visible) return;
 
-  m_visible = visible;
-  emit visibleChanged(visible);
+    m_visible = visible;
+    emit visibleChanged(visible);
 }
 
 bool MyClass::getVisible() const { return m_visible; }
--- QProperty/MyClass.h
+++ QProperty/MyClass.h
@@ -3,18 +3,18 @@
 #include <QObject>
 
 class MyClass : public QObject {
-  Q_OBJECT
-  Q_PROPERTY(
-      bool visible READ getVisible WRITE setVisible NOTIFY visibleChanged)
+    Q_OBJECT
+    Q_PROPERTY(
+        bool visible READ getVisible WRITE setVisible NOTIFY visibleChanged)
 public:
-  MyClass(QObject *parent = nullptr);
-  ~MyClass();
+    MyClass(QObject *parent = nullptr);
+    ~MyClass();
 
-  void setVisible(bool visible);
-  bool getVisible() const;
+    void setVisible(bool visible);
+    bool getVisible() const;
 signals:
-  void visibleChanged(bool visible);
+    void visibleChanged(bool visible);
 
 private:
-  bool m_visible;
+    bool m_visible;
 };
