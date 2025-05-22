1151c3701e41d06c893a27bd65fddf901be7f78a
Pang kuanxin <pang.kuanxin@cmsholding.com>
2025-05-22 16:02:33 +0800

    Reformat

--- LineChart/mainwindow.cpp
+++ LineChart/mainwindow.cpp
@@ -12,42 +12,42 @@ using namespace QtCharts;
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow) {
-  ui->setupUi(this);
-  QLineSeries *series = new QLineSeries;
-  series->setName("Time");
+    ui->setupUi(this);
+    QLineSeries *series = new QLineSeries;
+    series->setName("Time");
 
-  QDateTime xValue;
-  xValue.setDate(QDate(2012, 1, 18));
-  xValue.setTime(QTime(9, 34));
-  qreal yValue = 4;
-  series->append(xValue.toMSecsSinceEpoch(), yValue);
+    QDateTime xValue;
+    xValue.setDate(QDate(2012, 1, 18));
+    xValue.setTime(QTime(9, 34));
+    qreal yValue = 4;
+    series->append(xValue.toMSecsSinceEpoch(), yValue);
 
-  xValue.setDate(QDate(2013, 5, 11));
-  xValue.setTime(QTime(11, 14));
+    xValue.setDate(QDate(2013, 5, 11));
+    xValue.setTime(QTime(11, 14));
 
-  qreal yValue2 = 50;
-  series->append(xValue.toMSecsSinceEpoch(), yValue2);
+    qreal yValue2 = 50;
+    series->append(xValue.toMSecsSinceEpoch(), yValue2);
 
-  QChart *chart = new QChart;
-  chart->setAnimationOptions(QChart::SeriesAnimations);
+    QChart *chart = new QChart;
+    chart->setAnimationOptions(QChart::SeriesAnimations);
 
-  QDateTimeAxis *axisX = new QDateTimeAxis;
-  axisX->setFormat("yyyy-MM-dd");
-  axisX->setTickCount(2);
-  axisX->setLabelsAngle(-45);
+    QDateTimeAxis *axisX = new QDateTimeAxis;
+    axisX->setFormat("yyyy-MM-dd");
+    axisX->setTickCount(2);
+    axisX->setLabelsAngle(-45);
 
-  QValueAxis *axisY = new QValueAxis;
-  axisY->setTitleText("fdafdsa");
-  axisY->setRange(0, 100);
+    QValueAxis *axisY = new QValueAxis;
+    axisY->setTitleText("fdafdsa");
+    axisY->setRange(0, 100);
 
-  chart->addSeries(series);
-  chart->setAxisY(axisY, series);
-  chart->setAxisX(axisX, series);
+    chart->addSeries(series);
+    chart->setAxisY(axisY, series);
+    chart->setAxisX(axisX, series);
 
-  QChartView *chartView = new QChartView(chart);
-  chartView->setRenderHints(QPainter::Antialiasing);
+    QChartView *chartView = new QChartView(chart);
+    chartView->setRenderHints(QPainter::Antialiasing);
 
-  setCentralWidget(chartView);
+    setCentralWidget(chartView);
 }
 
 MainWindow::~MainWindow() { delete ui; }
