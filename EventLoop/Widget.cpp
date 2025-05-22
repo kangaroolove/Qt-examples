ec060539756582df8f1518474c63e4dd07a3b5dc
Pang kuanxin <pang.kuanxin@cmsholding.com>
2025-05-22 15:58:15 +0800

    Reformat

--- EventLoop/Widget.cpp
+++ EventLoop/Widget.cpp
@@ -3,11 +3,11 @@
 #include <QVBoxLayout>
 
 Widget::Widget(QWidget *parent) : QWidget(parent) {
-  auto layout = new QVBoxLayout(this);
-  auto button = new QPushButton("Quit event loop");
-  layout->addWidget(button);
-  connect(button, &QPushButton::clicked, this, &Widget::quitEventLoop);
-  resize(400, 300);
+    auto layout = new QVBoxLayout(this);
+    auto button = new QPushButton("Quit event loop");
+    layout->addWidget(button);
+    connect(button, &QPushButton::clicked, this, &Widget::quitEventLoop);
+    resize(400, 300);
 }
 
 Widget::~Widget() {}
--- EventLoop/main.cpp
+++ EventLoop/main.cpp
@@ -4,15 +4,15 @@
 #include <QEventLoop>
 
 int main(int argc, char *argv[]) {
-  QApplication a(argc, argv);
-  Widget widget;
-  widget.show();
+    QApplication a(argc, argv);
+    Widget widget;
+    widget.show();
 
-  QEventLoop loop;
-  QObject::connect(&widget, &Widget::quitEventLoop, &loop, &QEventLoop::quit);
-  qDebug() << "You have started the event loop";
-  loop.exec();
-  qDebug() << "You have quitted the event loop";
+    QEventLoop loop;
+    QObject::connect(&widget, &Widget::quitEventLoop, &loop, &QEventLoop::quit);
+    qDebug() << "You have started the event loop";
+    loop.exec();
+    qDebug() << "You have quitted the event loop";
 
-  return a.exec();
+    return a.exec();
 }
