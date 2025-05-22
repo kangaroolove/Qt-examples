e50e75dcd6bfef1a4cec78188cd1d9743bf2bfdc
Pang kuanxin <pang.kuanxin@cmsholding.com>
2025-05-22 16:06:27 +0800

    Reformat

--- QImageToQJsonValue/Widget.cpp
+++ QImageToQJsonValue/Widget.cpp
@@ -8,32 +8,32 @@
 #include <QVariant>
 
 Widget::Widget(QWidget *parent) : QWidget(parent) {
-  QImage image("D:/1.png");
+    QImage image("D:/1.png");
 
-  QJsonObject object;
-  object["image"] = QImageToQJsonValue(image);
-  QJsonDocument doc(object);
-  doc.toJson(QJsonDocument::Compact);
-  qDebug() << doc;
+    QJsonObject object;
+    object["image"] = QImageToQJsonValue(image);
+    QJsonDocument doc(object);
+    doc.toJson(QJsonDocument::Compact);
+    qDebug() << doc;
 
-  QImage convertImage = QJsonValueToQImage(object["image"]);
-  qDebug() << "convertImage.width() = " << convertImage.width();
-  qDebug() << "convertImage.height() = " << convertImage.height();
+    QImage convertImage = QJsonValueToQImage(object["image"]);
+    qDebug() << "convertImage.width() = " << convertImage.width();
+    qDebug() << "convertImage.height() = " << convertImage.height();
 }
 
 Widget::~Widget() {}
 
 QJsonValue Widget::QImageToQJsonValue(const QImage &image) {
-  QByteArray imageData;
-  QBuffer buffer(&imageData);
-  image.save(&buffer, "PNG");
+    QByteArray imageData;
+    QBuffer buffer(&imageData);
+    image.save(&buffer, "PNG");
 
-  return QJsonValue::fromVariant(buffer.data().toBase64());
+    return QJsonValue::fromVariant(buffer.data().toBase64());
 }
 
 QImage Widget::QJsonValueToQImage(const QJsonValue &value) {
-  QImage image;
-  image.loadFromData(QByteArray::fromBase64(value.toVariant().toByteArray()),
-                     "PNG");
-  return image;
+    QImage image;
+    image.loadFromData(QByteArray::fromBase64(value.toVariant().toByteArray()),
+                       "PNG");
+    return image;
 }
