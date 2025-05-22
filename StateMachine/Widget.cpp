eb252131163db679b1e1f56c3b89144a3e747e17
Pang kuanxin <pang.kuanxin@cmsholding.com>
2025-05-22 16:10:25 +0800

    Reformat

--- StateMachine/Widget.cpp
+++ StateMachine/Widget.cpp
@@ -10,76 +10,77 @@
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent), m_stateMachine(new QStateMachine(this)) {
-  initGui();
-  initStateMachine();
+    initGui();
+    initStateMachine();
 }
 
 Widget::~Widget() {}
 
 void Widget::initGui() {
-  m_btnS1 = new QPushButton("S1", this);
-  m_btnS2 = new QPushButton("S2", this);
-  m_btnS3 = new QPushButton("S3", this);
-  m_btnS4 = new QPushButton("SecondMain", this);
-  m_btnHistory = new QPushButton("History", this);
+    m_btnS1 = new QPushButton("S1", this);
+    m_btnS2 = new QPushButton("S2", this);
+    m_btnS3 = new QPushButton("S3", this);
+    m_btnS4 = new QPushButton("SecondMain", this);
+    m_btnHistory = new QPushButton("History", this);
 
-  auto buttonLayout = new QHBoxLayout();
-  buttonLayout->addWidget(m_btnS1);
-  buttonLayout->addWidget(m_btnS2);
-  buttonLayout->addWidget(m_btnS3);
-  buttonLayout->addWidget(m_btnS4);
-  buttonLayout->addWidget(m_btnHistory);
+    auto buttonLayout = new QHBoxLayout();
+    buttonLayout->addWidget(m_btnS1);
+    buttonLayout->addWidget(m_btnS2);
+    buttonLayout->addWidget(m_btnS3);
+    buttonLayout->addWidget(m_btnS4);
+    buttonLayout->addWidget(m_btnHistory);
 
-  m_label = new QLabel();
+    m_label = new QLabel();
 
-  auto mainLayout = new QVBoxLayout(this);
-  mainLayout->addLayout(buttonLayout);
-  mainLayout->addWidget(m_label);
+    auto mainLayout = new QVBoxLayout(this);
+    mainLayout->addLayout(buttonLayout);
+    mainLayout->addWidget(m_label);
 
-  resize(400, 300);
+    resize(400, 300);
 }
 
 void Widget::bindConnections() {}
 
 void Widget::initStateMachine() {
-  auto rootState = new QState();
-  auto mainState = new QState(rootState);
-  auto secondState = new QState();
+    auto rootState = new QState();
+    auto mainState = new QState(rootState);
+    auto secondState = new QState();
 
-  auto s1 = new QState(mainState);
-  auto s2 = new QState(mainState);
-  auto s3 = new QState(mainState);
+    auto s1 = new QState(mainState);
+    auto s2 = new QState(mainState);
+    auto s3 = new QState(mainState);
 
-  auto historyState = new QHistoryState(mainState);
+    auto historyState = new QHistoryState(mainState);
 
-  s1->assignProperty(m_label, "text", "In state s1");
-  s2->assignProperty(m_label, "text", "In state s2");
-  s3->assignProperty(m_label, "text", "In state s3");
+    s1->assignProperty(m_label, "text", "In state s1");
+    s2->assignProperty(m_label, "text", "In state s2");
+    s3->assignProperty(m_label, "text", "In state s3");
 
-  mainState->setInitialState(s1);
-  rootState->setInitialState(mainState);
+    mainState->setInitialState(s1);
+    rootState->setInitialState(mainState);
 
-  connect(mainState, &QState::entered, this,
-          [this] { qDebug() << "Enter main state"; });
-  connect(mainState, &QState::exited, this,
-          [this] { qDebug() << "Exit main state"; });
-  connect(secondState, &QState::entered, this,
-          [this] { qDebug() << "Enter secondState state"; });
-  connect(secondState, &QState::exited, this,
-          [this] { qDebug() << "Exit secondState state"; });
+    connect(mainState, &QState::entered, this,
+            [this] { qDebug() << "Enter main state"; });
+    connect(mainState, &QState::exited, this,
+            [this] { qDebug() << "Exit main state"; });
+    connect(secondState, &QState::entered, this,
+            [this] { qDebug() << "Enter secondState state"; });
+    connect(secondState, &QState::exited, this,
+            [this] { qDebug() << "Exit secondState state"; });
 
-  rootState->addTransition(m_btnS1, &QPushButton::clicked, s1);
-  rootState->addTransition(m_btnS2, &QPushButton::clicked, s2);
-  rootState->addTransition(m_btnS3, &QPushButton::clicked, s3);
-  rootState->addTransition(m_btnS4, &QPushButton::clicked, secondState);
-  secondState->addTransition(m_btnHistory, &QPushButton::clicked, historyState);
+    rootState->addTransition(m_btnS1, &QPushButton::clicked, s1);
+    rootState->addTransition(m_btnS2, &QPushButton::clicked, s2);
+    rootState->addTransition(m_btnS3, &QPushButton::clicked, s3);
+    rootState->addTransition(m_btnS4, &QPushButton::clicked, secondState);
+    secondState->addTransition(m_btnHistory, &QPushButton::clicked,
+                               historyState);
 
-  connect(s1, &QState::entered, this, [this] { qDebug() << "Enter s1"; });
-  connect(s2, &QState::entered, this, [this] { qDebug() << "Enter s2"; });
-  connect(s3, &QState::entered, this, [this] { qDebug() << "Enter s3"; });
+    connect(s1, &QState::entered, this, [this] { qDebug() << "Enter s1"; });
+    connect(s2, &QState::entered, this, [this] { qDebug() << "Enter s2"; });
+    connect(s3, &QState::entered, this, [this] { qDebug() << "Enter s3"; });
 
-  m_stateMachine->addState(rootState);
-  m_stateMachine->addState(secondState);
-  m_stateMachine->setInitialState(rootState);
-  m_stateMachine->start();
+    m_stateMachine->addState(rootState);
+    m_stateMachine->addState(secondState);
+    m_stateMachine->setInitialState(rootState);
+    m_stateMachine->start();
 }
