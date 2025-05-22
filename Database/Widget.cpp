a1c9b59c8740708bf2d4fd31b8e210dcc7951fe7
Pang kuanxin <pang.kuanxin@cmsholding.com>
2025-05-22 15:55:03 +0800

    Reformat

--- Database/Widget.cpp
+++ Database/Widget.cpp
@@ -10,48 +10,49 @@
 #include <qsqlerror.h>
 
 Widget::Widget(QWidget *parent) : QWidget(parent), m_tableView(new QTableView) {
-  initGui();
-  auto database = QSqlDatabase::addDatabase("QSQLITE", "my connection");
-  database.setDatabaseName("testDatabase.db");
-  if (!database.open()) {
-    qDebug() << "Cannot open the database";
-    qDebug() << database.lastError().text();
-    return;
-  }
+    initGui();
+    auto database = QSqlDatabase::addDatabase("QSQLITE", "my connection");
+    database.setDatabaseName("testDatabase.db");
+    if (!database.open()) {
+        qDebug() << "Cannot open the database";
+        qDebug() << database.lastError().text();
+        return;
+    }
 
-  if (!isDatabaseCreated())
-    createDatabase(database);
+if (!isDatabaseCreated()) createDatabase(database);
 
-  showData(database);
+showData(database);
 }
 
 Widget::~Widget() {}
 
 void Widget::createDatabase(const QSqlDatabase &database) {
-  QStringList cmds;
-  cmds << "CREATE TABLE Users (UserID INTEGER PRIMARY KEY AUTOINCREMENT, "
-          "Username VARCHAR(50) NOT NULL UNIQUE, Email VARCHAR(100) NOT NULL "
-          "UNIQUE);";
-  cmds << ("INSERT INTO Users (Username, Email) VALUES ('a', 'a@gmail.com');");
-  cmds << ("INSERT INTO Users (Username, Email) VALUES ('b', 'b@gmail.com');");
-  QSqlQuery sql(database);
-  for (auto &cmd : cmds)
-    qDebug() << "cmd = " << cmd << ", result = " << sql.exec(cmd);
+    QStringList cmds;
+    cmds << "CREATE TABLE Users (UserID INTEGER PRIMARY KEY AUTOINCREMENT, "
+            "Username VARCHAR(50) NOT NULL UNIQUE, Email VARCHAR(100) NOT NULL "
+            "UNIQUE);";
+    cmds
+        << ("INSERT INTO Users (Username, Email) VALUES ('a', 'a@gmail.com');");
+    cmds
+        << ("INSERT INTO Users (Username, Email) VALUES ('b', 'b@gmail.com');");
+    QSqlQuery sql(database);
+    for (auto &cmd : cmds)
+        qDebug() << "cmd = " << cmd << ", result = " << sql.exec(cmd);
 }
 
 bool Widget::isDatabaseCreated() {
-  QFileInfo fileInfo(QApplication::applicationDirPath() + "/testDatabase.db");
-  return fileInfo.size() > 0 ? true : false;
+    QFileInfo fileInfo(QApplication::applicationDirPath() + "/testDatabase.db");
+    return fileInfo.size() > 0 ? true : false;
 }
 
 void Widget::showData(const QSqlDatabase &database) {
-  auto model = new QSqlTableModel(this, database);
-  model->setTable("Users");
-  model->select();
-  m_tableView->setModel(model);
+    auto model = new QSqlTableModel(this, database);
+    model->setTable("Users");
+    model->select();
+    m_tableView->setModel(model);
 }
 
 void Widget::initGui() {
-  auto layout = new QVBoxLayout(this);
-  layout->addWidget(m_tableView);
+    auto layout = new QVBoxLayout(this);
+    layout->addWidget(m_tableView);
 }
