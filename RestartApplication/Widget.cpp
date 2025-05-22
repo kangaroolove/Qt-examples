e0576c2496e2b57431b9dd3ad2b2cd65f4fe6db9
Pang kuanxin <pang.kuanxin@cmsholding.com>
2025-05-22 16:07:49 +0800

    Reformat

--- RestartApplication/Widget.cpp
+++ RestartApplication/Widget.cpp
@@ -8,16 +8,16 @@
 #include <QVBoxLayout>
 
 Widget::Widget(QWidget *parent) : QWidget(parent) {
-  auto layout = new QVBoxLayout(this);
-  QPushButton *restartButton = new QPushButton("Restart", this);
-  connect(restartButton, &QPushButton::clicked, this, [this] {
-    QApplication::quit();
-    qDebug() << QProcess::startDetached(QApplication::applicationFilePath(),
-                                        QStringList());
-  });
-  layout->addWidget(restartButton);
+    auto layout = new QVBoxLayout(this);
+    QPushButton *restartButton = new QPushButton("Restart", this);
+    connect(restartButton, &QPushButton::clicked, this, [this] {
+        QApplication::quit();
+        qDebug() << QProcess::startDetached(QApplication::applicationFilePath(),
+                                            QStringList());
+    });
+    layout->addWidget(restartButton);
 
-  this->resize(400, 300);
+    this->resize(400, 300);
 }
 
 Widget::~Widget() { qDebug() << "~Widget"; }
