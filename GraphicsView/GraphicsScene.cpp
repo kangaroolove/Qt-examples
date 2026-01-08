0a915e6ffeb5336d62a2f16c0a41d496bbf484a4
Pang KuanXin <pang.kuanxin@cmsholding.com>
2026-01-08 15:27:38 +0800

    Refactor the widget

--- GraphicsView/GraphicsScene.cpp
+++ GraphicsView/GraphicsScene.cpp
@@ -1,18 +1,59 @@
 #include "GraphicsScene.h"
 
+#include <QDebug>
 #include <QGraphicsProxyWidget>
+#include <QGraphicsSceneMouseEvent>
+#include <QLabel>
+#include <QPainter>
 
 #include "Widget.h"
 
 GraphicsScene::GraphicsScene(QObject* parent) : QGraphicsScene(parent) {
-    QPixmap pixmap("D:/1.jpg");
-    addPixmap(pixmap);
+    QPixmap pixmap("D:/2.jpg");
+    auto pixmapItem = addPixmap(pixmap);
+    pixmapItem->setTransformationMode(Qt::SmoothTransformation);
 
     addText("Hello, world");
 
     auto text2 = addText("Second Text");
     text2->setPos(50, 50);
+}
 
-    auto widget = new Widget;
-    m_zoomWidget = addWidget(widget);
+void GraphicsScene::mouseMoveEvent(QGraphicsSceneMouseEvent* mouseEvent) {
+    // This one always return 0
+    // qDebug() << "mouseEvent->pos = " << mouseEvent->pos();
+    auto pos = mouseEvent->scenePos();
+    qDebug() << "mouseEvent->scenePos" << pos;
+
+    // m_magnifierProxyWidget->setVisible(false);
+
+    // // if ((pos.x() >= 200 / 2 || pos.x() <= 1280 - (200 / 2)) &&
+    // //         pos.y() >= 100 / 2 ||
+    // //     pos.y() <= 720 - (100 / 2))
+    // m_magnifierProxyWidget->setPos(pos.x() - (200 / 2), pos.y() - (100 / 2));
+
+    // QRect rect(std::round(pos.x() - (200 / 2)), std::round(pos.y() - (100 /
+    // 2)),
+    //            200, 100);
+
+    // qDebug() << "Rect = " << rect;
+
+    // // QRectF rect(50, 50, 200, 100);
+    // QImage image(rect.size(), QImage::Format_RGB32);
+    // image.fill(Qt::transparent);  // clear the background
+
+    // // 3. Initialize a QPainter on the image
+    // QPainter painter(&image);
+    // painter.setRenderHint(QPainter::Antialiasing);
+    // // painter.setRenderHint(QPainter::SmoothPixmapTransform);
+
+    // qDebug() << "image.rect()" << image.rect();
+
+    // render(&painter, image.rect(), rect);
+    // painter.end();
+
+    // m_magnifierWidget->setPixmap(QPixmap::fromImage(image));
+    // m_magnifierProxyWidget->setVisible(true);
+
+    QGraphicsScene::mouseMoveEvent(mouseEvent);
 }
--- GraphicsView/GraphicsScene.h
+++ GraphicsView/GraphicsScene.h
@@ -2,11 +2,17 @@
 
 #include <QGraphicsScene>
 
+class QLabel;
+
 class GraphicsScene : public QGraphicsScene
 {
 public:
     GraphicsScene(QObject* parent = nullptr);
 
+protected:
+    void mouseMoveEvent(QGraphicsSceneMouseEvent* mouseEvent);
+
 private:
-    QGraphicsProxyWidget* m_zoomWidget;
+    QGraphicsProxyWidget* m_magnifierProxyWidget;
+    QLabel* m_magnifierWidget;
 };
