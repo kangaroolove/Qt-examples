d423e5d4b398ae535d7b052f04918495325c5749
Pang KuanXin <pang.kuanxin@cmsholding.com>
2026-05-05 14:18:19 +0800

    Refactor the code

--- LocalIPC/client/ClientWorker.cpp
+++ LocalIPC/client/ClientWorker.cpp
@@ -5,9 +5,15 @@
 #include <QLocalSocket>
 #include <QThread>
 
-ClientWorker::ClientWorker(QObject *parent) : QObject(parent) {}
+ClientWorker::ClientWorker(QObject *parent)
+    : QObject(parent), m_socket(nullptr) {}
 
 void ClientWorker::sendMessage(const QString &msg) {
+    if (!m_socket) {
+        qDebug() << "Failed to send message. Please connect the server first!";
+        return;
+    }
+
     QByteArray data;
     QDataStream out(&data, QIODevice::WriteOnly);
     out.setVersion(QDataStream::Qt_5_12);
@@ -26,10 +32,10 @@ void ClientWorker::readyRead() {
 }
 
 void ClientWorker::connectToServer(const QString &name) {
-    m_socket = new QLocalSocket(this);
-    connect(m_socket, &QLocalSocket::readyRead, this, &ClientWorker::readyRead);
+    if (!m_socket) {
+        m_socket = new QLocalSocket(this);
+        connect(m_socket, &QLocalSocket::readyRead, this,
+                &ClientWorker::readyRead);
+    }
     m_socket->connectToServer(name);
-
-    QEventLoop loop;
-    loop.exec();
 }
