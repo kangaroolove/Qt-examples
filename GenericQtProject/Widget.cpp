51e5ddb0e86afba00bc8c2e91325797c06dcd10f
Pang KuanXin <pang.kuanxin@cmsholding.com>
2026-05-11 17:21:54 +0800

    Refactor the code

--- GenericQtProject/Widget.cpp
+++ GenericQtProject/Widget.cpp
@@ -4,12 +4,4 @@
 #include <QDebug>
 #include <QListView>
 
-Widget::Widget(QWidget *parent) : QWidget(parent) {
-    auto box = new QComboBox(this);
-    box->addItems({"AAAAAAAAAAAA", "BBBBBBBBBBBB"});
-
-    auto listView = new QListView();
-    listView->setStyleSheet(
-        "QListView{font-size:30px;} QListView::item{height: 60px;}");
-    box->setView(listView);
-}
+Widget::Widget(QWidget *parent) : QWidget(parent) {}
