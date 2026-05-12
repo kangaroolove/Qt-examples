88a966218e70f9b0322b0836d934a96ceed5ef9c
Pang KuanXin <pang.kuanxin@cmsholding.com>
2026-05-12 09:06:57 +0800

    Update this widget

--- GraphicsViewLayout/Widget.cpp
+++ GraphicsViewLayout/Widget.cpp
@@ -1,7 +1,23 @@
 #include "Widget.h"
 
-#include <QComboBox>
 #include <QDebug>
-#include <QListView>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QPushButton>
+#include <QVBoxLayout>
 
-Widget::Widget(QWidget *parent) : QWidget(parent) {}
+Widget::Widget(QWidget *parent)
+    : QWidget(parent),
+      m_graphicsView(new QGraphicsView(this)),
+      m_graphicsSecne(new QGraphicsScene),
+      m_switchButton(new QPushButton("Switch", this)) {
+    initGui();
+}
+
+void Widget::initGui() {
+    m_graphicsView->setScene(m_graphicsSecne);
+
+    auto layout = new QVBoxLayout(this);
+    layout->addWidget(m_graphicsView);
+    layout->addWidget(m_switchButton);
+}
--- GraphicsViewLayout/Widget.h
+++ GraphicsViewLayout/Widget.h
@@ -2,8 +2,18 @@
 
 #include <QWidget>
 
+class QGraphicsView;
+class QGraphicsScene;
+class QPushButton;
 class Widget : public QWidget {
     Q_OBJECT
 public:
     Widget(QWidget *parent = nullptr);
+
+private:
+    void initGui();
+
+    QGraphicsView* m_graphicsView;
+    QGraphicsScene* m_graphicsSecne;
+    QPushButton* m_switchButton;
 };
